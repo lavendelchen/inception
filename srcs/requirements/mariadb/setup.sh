68b3c62373ca96e08b10845671e94c8109782533
Lavendelchen <lavendel02sh@gmail.com>
2023-08-06 19:20:36 +0200

    mariadb actually done hahahaahahah

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -2,14 +2,13 @@
 
 ( service mariadb start & ) | grep -q "Service is active" || true
 
-mysql -u root << EOF
+mariadb -u root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
 EOF
-#MARIADB
 
 sleep 5
 
