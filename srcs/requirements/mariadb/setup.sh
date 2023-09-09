6857216c4c5f98ae2cf2b258f12835a4dd62160b
Lavendelchen <lavendel02sh@gmail.com>
2023-09-09 17:44:35 +0200

    some cosmentic changes, but mostly "VOLUME WAS PROBLEM & NOW WE FIXED IT"

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -4,10 +4,14 @@
 set -ex
 
 # Start mariadb so we can do the setup
-( service mariadb start & ) | grep -q "Service is active" || true
+( service mariadb start & ) | grep -q "Service is active" || true #??
+
+sleep 3
+
+service mariadb status
 
 # Create all necessary users etc (if they haven't been created already)
-mariadb -u root << EOF
+mariadb -v -u root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
@@ -15,10 +19,9 @@ GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
 EOF
 
-# Why???
 sleep 5
 
-# Why???
+# stop mariadb
 ( service mariadb stop & ) | grep -q "Service is inactive" || true
 
 # Run mysql/mariadb
